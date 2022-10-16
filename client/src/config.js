7879d961804fa42c8a045e6ec9668a520857f9b9
Mitesh Tank <codewithmitesh@gmail.com>
2022-10-16 15:51:44 +0530

    Updated base Url

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://nibtube.herokuapp.com",
+    baseURL: "https://nibtube.herokuapp.com/api",
     withCredentials: true,
     // headers: {
     //     'Access-Control-Allow-Origin': 'http://localhost:5000', 'Content-Type': 'application/json'
